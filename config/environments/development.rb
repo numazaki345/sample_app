c82b91ad3c5f8548d0c7df7decee82bfaba98d1d
numazaki345 <ryo.091979@gmail.com>
2022-05-22 07:57:22 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'c184f4395b144a938adeb862f8fe5618.vfs.cloud9.ap-southeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
